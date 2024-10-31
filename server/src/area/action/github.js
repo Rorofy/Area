0e0697caed5e9cb87e851de0c64d491fc315f09c
Francois Zhao <francois.zhao.fz@gmail.com>
2024-10-31 09:48:44 +0100

    remove tokens

--- server/src/area/action/github.js
+++ server/src/area/action/github.js
@@ -1,25 +1,26 @@
-const axios = require("axios")
+const axios = require("axios");
 
 async function getGithubPush(repository, token) {
   const URL = `https://api.github.com/repos/${repository}`;
-  const after = Date.now()
+  const after = Date.now();
   try {
     const repo = await axios.get(URL, {
       headers: {
-        Authorization: "token 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",
+        Authorization:
+          "token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
       },
-    })
-    const res = []
+    });
+    const res = [];
     if (after - Date.parse(repo.data.pushed_at) > 60000) {
       return {
         message: "",
-        data: []
-      }
+        data: [],
+      };
     } else {
       return {
         message: "Un push a été fait sur le repo " + repository,
-        data: [true]
-      }
+        data: [true],
+      };
     }
   } catch (e) {
     //console.error(e)
--- server/src/area/reaction/sms.js
+++ server/src/area/reaction/sms.js
@@ -1,16 +1,16 @@
-const accountSid = 'AC65accc517b42c556354365bb8b0e5368'; 
-const authToken = '61365ea21fc10c0ebc654b2508ded2e0'; 
-const client = require('twilio')(accountSid, authToken); 
- 
+const accountSid = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const authToken = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const client = require("twilio")(accountSid, authToken);
+
 async function sendSMS(message, number) {
-  client.messages 
-        .create({ 
-           body: message,  
-           messagingServiceSid: 'MGe365e42111b1b90ddebc15d646f5a677',      
-           to: number 
-         }) 
-        .then(message => console.log(message.sid)) 
-        .done();
+  client.messages
+    .create({
+      body: message,
+      messagingServiceSid: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      to: number,
+    })
+    .then((message) => console.log(message.sid))
+    .done();
 }
 
-module.exports.sendSMS = sendSMS
+module.exports.sendSMS = sendSMS;
